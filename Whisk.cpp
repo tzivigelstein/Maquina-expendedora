0e61e016d814a942db02024da77faeab87d3a86e
Tzivi <gelsteintzivisantiago@gmail.com>
2021-08-08 18:28:41 -0300

    Init Whisk class

---
+++ Whisk.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+class Whisk
+{
+private:
+    int pin;
+
+public:
+    Whisk(int pin)
+    {
+        this->pin = pin;
+    }
+
+    void start()
+    {
+    }
+
+    void stop()
+    {
+    }
+};
