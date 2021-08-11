4753a1c441aa0312fed659935f9bd1e28d911695
Tzivi <gelsteintzivisantiago@gmail.com>
2021-08-11 20:01:38 -0300

    Add better naming and two more coffee options

--- maquina-expendedora.ino
+++ maquina-expendedora.ino
@@ -6,21 +6,26 @@ const int smallCupTime = 15000;
 int cupTime = smallCupTime;
 
 //Motors
-const int motorWatter = 13;
+const int motorWater = 13;
 const int motorCoffe = 12;
 const int motorMilk = 11;
+const int motorChocolate = 10;
 
 //Temperature
 const int temperatureLimit = 90;
 
 //Buttons
-const int buttonAPin = 10;
-const int buttonBPin = 9;
-const int buttonCPin = 8;
+const int buttonBlackCoffeePin = A2;
+const int buttonLongBlackCoffeePin = A1;
+const int buttonWhiteCoffeePin = 10;
+const int buttonLattePin = 9;
+const int buttonCappuccinoPin = 8;
 
-Button buttonA(buttonAPin);
-Button buttonB(buttonBPin);
-Button buttonC(buttonCPin);
+Button buttonBlackCoffee(buttonBlackCoffeePin);
+Button buttonLongBlackCoffee(buttonLongBlackCoffeePin);
+Button buttonWhiteCoffee(buttonWhiteCoffee);
+Button buttonLatte(buttonLattePin);
+Button buttonCappuccino(buttonCappuccinoPin);
 
 Display display;
 
@@ -29,55 +34,101 @@ void setup()
   Serial.begin(9600);
   display.init();
 
-  for (int i = 0; i < 14; i++) {
+  for (int i = 0; i < 14; i++)
+  {
     pinMode(i, OUTPUT);
   }
 }
 
 void loop()
 {
-  if (buttonA.isPressed())
+  if (buttonBlackCoffee.isPressed())
   {
-    //Black coffee
-
-    digitalWrite(motorWatter, HIGH);
-    delay(1000);
-    digitalWrite(motorCoffe, HIGH);
-    delay(4000);
-    digitalWrite(motorCoffe, LOW);
-    delay(10000);
-    digitalWrite(motorWatter, LOW);
+    blackCoffee();
   }
 
-  if (buttonB.isPressed())
+  if (buttonLongBlackCoffee.isPressed())
   {
-    //Latte
-
-    digitalWrite(motorWatter, HIGH);
-    delay(1000);
-    digitalWrite(motorCoffe, HIGH);
-    delay(4000);
-    digitalWrite(motorCoffe, LOW);
-    digitalWrite(motorMilk, HIGH);
-    delay(8000);
-    digitalWrite(motorMilk, LOW);
-    delay(2000);
-    digitalWrite(motorWatter, LOW);
+    longBlackCoffee();
   }
 
-  if (buttonC.isPressed())
+  if (buttonLatte.isPressed())
   {
-    //White coffee
-
-    digitalWrite(motorWatter, HIGH);
-    delay(1000);
-    digitalWrite(motorCoffe, HIGH);
-    delay(4000);
-    digitalWrite(motorCoffe, LOW);
-    digitalWrite(motorMilk, HIGH);
-    delay(4000);
-    digitalWrite(motorMilk, LOW);
-    delay(9500);
-    digitalWrite(motorWatter, LOW);
+    latte();
   }
+
+  if (buttonWhiteCoffee.isPressed())
+  {
+    whiteCoffee();
+  }
+
+  if(buttonCappuccino.isPressed()){
+    cappuccino();
+  }
+}
+
+void blackCoffee()
+{
+  digitalWrite(motorWater, HIGH);
+  delay(1000);
+  digitalWrite(motorCoffe, HIGH);
+  delay(3000);
+  digitalWrite(motorCoffe, LOW);
+  delay(10000);
+  digitalWrite(motorWater, LOW);
+}
+
+void longBlackCoffee()
+{
+  digitalWrite(motorWater, HIGH);
+  delay(1000);
+  digitalWrite(motorCoffe, HIGH);
+  delay(4000);
+  digitalWrite(motorCoffe, LOW);
+  delay(10000);
+  digitalWrite(motorWater, LOW);
+}
+
+void latte()
+{
+  digitalWrite(motorWater, HIGH);
+  delay(1000);
+  digitalWrite(motorCoffe, HIGH);
+  delay(4000);
+  digitalWrite(motorCoffe, LOW);
+  digitalWrite(motorMilk, HIGH);
+  delay(8000);
+  digitalWrite(motorMilk, LOW);
+  delay(2000);
+  digitalWrite(motorWater, LOW);
+}
+
+void whiteCoffee()
+{
+  digitalWrite(motorWater, HIGH);
+  delay(1000);
+  digitalWrite(motorCoffe, HIGH);
+  delay(4000);
+  digitalWrite(motorCoffe, LOW);
+  digitalWrite(motorMilk, HIGH);
+  delay(4000);
+  digitalWrite(motorMilk, LOW);
+  delay(9500);
+  digitalWrite(motorWater, LOW);
+}
+
+void cappuccino (){
+  digitalWrite(motorWater, HIGH);
+  delay(1000);
+  digitalWrite(motorCoffe, HIGH);
+  delay(4000);
+  digitalWrite(motorCoffe, LOW);
+  digitalWrite(motorMilk, HIGH);
+  delay(2000);
+  digitalWrite(motorChocolate, HIGH);
+  delay(6000);
+  digitalWrite(motorMilk, LOW);
+  digitalWrite(motorChocolate, LOW);
+  delay(2000);
+  digitalWrite(motorWater, LOW);
 }
